8f6d0dbe95833f7bc9d871ee732646f2640e429d
ahaydeid <adi.hadi270@gmail.com>
2026-07-18 12:31:44 +0700

    perbarui tipe data product_id di migration cart_items menjadi TEXT

--- supabase/migrations/04_create_cart_items_table.sql
+++ supabase/migrations/04_create_cart_items_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS public.cart_items (
     id UUID PRIMARY KEY DEFAULT generate_uuid_v7(),
     user_id UUID NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE,
-    product_id UUID NOT NULL REFERENCES public.products(id) ON DELETE CASCADE,
+    product_id TEXT NOT NULL REFERENCES public.products(id) ON DELETE CASCADE,
     variant TEXT NOT NULL DEFAULT '',
     quantity INT NOT NULL DEFAULT 1 CHECK (quantity > 0),
     checked BOOLEAN NOT NULL DEFAULT true,
